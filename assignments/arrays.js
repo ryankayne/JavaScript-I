7b01c90e03de5bf90673811faf00d12fc4811633
ryankayne <coope1rk@gmail.com>
2019-10-14 19:59:56 -0400

    Finished function conversions

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -98,6 +98,7 @@ for (let i = 0; i < inventory.length; i++) {
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
+
 for (let i = 0; i < inventory.length; i++) {
   carYears.push(inventory[i].car_year); 
 }
